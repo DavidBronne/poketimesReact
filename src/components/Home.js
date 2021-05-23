74a68192e8e691d7a260e5d23f1b34d6f7477b33
David Bronne <davidbronne@gmail.com>
2021-05-23 13:15:52 +0200

    componentDidMount - Axios - map

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-
-const Home = () => {
-    return (
-        <div className="container">
+class Home extends Component {
+        
+    state = {
+        posts:[]
+    }
+    
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res=> {
+            console.log(res.data.slice(0,10));
             
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, tenetur odit hic excepturi laborum nihil quas aspernatur, nisi mollitia consequuntur nostrum officia fuga aliquid unde quia dolorum dolores eveniet deleniti!</p>
-        </div>
-    )
+                this.setState({
+                    posts : res.data.slice(0,10)
+                })
+        })
+
+    }
+   
+    
+
+
+    render () {
+        
+        const {posts} = this.state;
+
+        const postsList = posts.length ? (
+            posts.map( post => {
+                return (
+
+                    <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <h1 className="card-title">{post.title}</h1>
+                        <p className="">{post.body}</p>
+                    </div>
+                </div>
+                    )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+
+            return (
+                <div className="container"> 
+                    <h4 className="center">Home</h4>
+                    <p>{postsList}</p>
+                </div>
+        )
+    } 
 }
  export default Home
