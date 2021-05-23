e8e5b63b040d173724fb4f7465149d040b4c9770
David Bronne <davidbronne@gmail.com>
2021-05-23 11:40:20 +0200

    redirect

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+  /*   setTimeout(() => {
+        props.history.push('/about')
+    }, 2000) */
+
     return (
+        
+        
         <div className="container">
             <h4 className="center">Contact</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, tenetur odit hic excepturi laborum nihil quas aspernatur, nisi mollitia consequuntur nostrum officia fuga aliquid unde quia dolorum dolores eveniet deleniti!</p>
